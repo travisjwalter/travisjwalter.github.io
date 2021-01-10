73778c230b2c8659ed456e67cbc619ff9f1f7ac2
tjwalter <tjwalter@ncsu.edu>
2021-01-09 21:44:21 -0500

    Trying to make php work!

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+$GithubRawURI= "https://raw.githubusercontent.com/travisjwalter/travisjwalter.github.io/master/assets/mail/contact_me.php";
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
